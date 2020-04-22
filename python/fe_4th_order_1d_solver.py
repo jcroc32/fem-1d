c3eeb35d32001d1e80a7f6d1ed7f72106159ece9
jcroc32 <jcroc32@gmail.com>
2020-04-22 17:49:35 -0500

    wip 1.1

--- python/fe_4th_order_1d_solver.py
+++ python/fe_4th_order_1d_solver.py
@@ -5,8 +5,9 @@ import global_matrix
 def solve(N, c, f, BC, x):
 	h = 1./(N-1)
 	elements, A0, A1, A2 = local_matrix.create(h)
-	A = global_matrix.create(N, A0, A1, A2, c)
-	
+	A = global_matrix.createA(N, A0, A1, A2, c)
+	A0 = global_matrix.createA0(N, A0)
+	f = [f(i//2/N)[i%2] for i in range(2*N+2)]
 	
 	# x = scipy.sparse.linalg.spsolve(A, b)
 	
--- python/global_matrix.py
+++ python/global_matrix.py
@@ -1,12 +1,21 @@
 import scipy.sparse
 
-def create(N, A0, A1, A2, c):
-	A0 = N*[i for a in A0 for i in a]
-	A1 = N*[i for a in A1 for i in a]
-	A2 = N*[i for a in A2 for i in a]
+def createA(N, A0, A1, A2, c):
+	A0 = N*[c(0)[0]*i for a in A0 for i in a]
+	A1 = N*[c(0)[1]*i for a in A1 for i in a]
+	A2 = N*[c(0)[2]*i for a in A2 for i in a]
 	row = [i  for j in range(2, 2*N+2, 2)  for i in range(j-2, j+2)  for k in range(4)]
 	column = [i  for j in range(2, 2*N+2, 2)  for k in range(4)  for i in range(j-2, j+2)]
 	
 	A = scipy.sparse.coo_matrix((data, (row, column)), shape=(2*N+2, 2*N+2))
+	A = A.tobsr()
+	return A
+
+def createA0(N, A0):
+	A0 = N*[i for a in A0 for i in a]
+	row = [i  for j in range(2, 2*N+2, 2)  for i in range(j-2, j+2)  for k in range(4)]
+	column = [i  for j in range(2, 2*N+2, 2)  for k in range(4)  for i in range(j-2, j+2)]
+	
+	A = scipy.sparse.coo_matrix((A0, (row, column)), shape=(2*N+2, 2*N+2))
 	A = A.tobsr()
 	return A
