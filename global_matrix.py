eb871db76a39d2aa99b320ed31636d840bce59cc
jcroc <jcroc@tamu.edu>
2020-05-04 00:58:52 -0500

    fixed sign on u^(2)

--- global_matrix.py
+++ global_matrix.py
@@ -4,24 +4,34 @@ import scipy.sparse
 def create(N, c, f, BC, A0, A1, A2):
 	# make global matrix A s.t. A*U = F
 	# remove A[0:2,:],  A[:,0:2],  A[-2:,:], A[:,-2:] to add to RHS later
-	A0_ = 	[ A0[2,2], A0[2,3], A0[3,2], A0[3,3] ] + (N-2)*[ i for a in A0 for i in a ] + [ A0[0,0], A0[0,1], A0[1,0], A0[1,1] ]
+	A0_ = 	[ A0[2,2], A0[2,3], A0[3,2], A0[3,3] ] + \
+			(N-2)*[ i for a in A0 for i in a ] + \
+			[ A0[0,0], A0[0,1], A0[1,0], A0[1,1] ]
 	
-	A1_ = 	[ A1[2,2], A1[2,3], A1[3,2], A1[3,3] ] + (N-2)*[ i for a in A1 for i in a ] + [ A1[0,0], A1[0,1], A1[1,0], A1[1,1] ]
+	A1_ = 	[ A1[2,2], A1[2,3], A1[3,2], A1[3,3] ] + \
+			(N-2)*[ i for a in A1 for i in a ] + \
+			[ A1[0,0], A1[0,1], A1[1,0], A1[1,1] ]
 	
-	A2_ = 	[ A2[2,2], A2[2,3], A2[3,2], A2[3,3] ] + (N-2)*[ i for a in A2 for i in a ] + [ A2[0,0], A2[0,1], A2[1,0], A2[1,1] ]
+	A2_ = 	[ A2[2,2], A2[2,3], A2[3,2], A2[3,3] ] + \
+			(N-2)*[ i for a in A2 for i in a ] + \
+			[ A2[0,0], A2[0,1], A2[1,0], A2[1,1] ]
 	
-	A = 	[ c[0]*A0_[i] + c[1]*A1_[i] + c[2]*A2_[i] for i in range(len(A0_)) ]
+	A = 	[ c[0]*A0_[i] - c[1]*A1_[i] + c[2]*A2_[i] for i in range(len(A0_)) ]
 	
-	row = 	[0, 0, 1, 1] + [ i  for j in range(2, 2*N-2, 2) for i in range(j-2, j+2)  for k in range(4) ] + [2*N-4, 2*N-4, 2*N-3, 2*N-3]
+	row = 	[0, 0, 1, 1] + \
+			[ i  for j in range(2, 2*N-2, 2) for i in range(j-2, j+2)  for k in range(4) ] + \
+			[2*N-4, 2*N-4, 2*N-3, 2*N-3]
 	
-	col = 	[0, 1, 0, 1] + [ i  for j in range(2, 2*N-2, 2) for k in range(4)  for i in range(j-2, j+2) ] + [2*N-4, 2*N-3, 2*N-4, 2*N-3]
+	col = 	[0, 1, 0, 1] + \
+			[ i  for j in range(2, 2*N-2, 2) for k in range(4)  for i in range(j-2, j+2) ] + \
+			[2*N-4, 2*N-3, 2*N-4, 2*N-3]
 	
 	A = scipy.sparse.coo_matrix( (A, (row, col)), shape=(2*N-2, 2*N-2))
 	A = A.tocsr()
 	
 	# make F for A*U = F
 	# move A[0:2,:],  A[:,0:2],  A[-2:,:], A[:,-2:] to add to RHS
-	A_ = c[0]*A0 + c[1]*A1 + c[2]*A2
+	A_ = c[0]*A0 - c[1]*A1 + c[2]*A2
 	bc1 = BC[0]*A_[:,0] + BC[1]*A_[:,1]
 	bc2 = BC[2]*A_[:,2] + BC[3]*A_[:,3]
 	# create A0 to integrate f*v
--- matlab/global_matrix.m
+++ matlab/global_matrix.m
@@ -10,7 +10,7 @@ function [A, F] = global_matrix(N, c, f, BC, A0, A1, A2)
 	A2_ = 	[ [ A2(3,3), A2(3,4), A2(4,3), A2(4,4) ] ...
 			repmat(A2'(:)',1,N-2) ...
 			[ A2(1,1), A2(1,2), A2(2,1), A2(2,2) ] ];
-	A = 	c(1)*A0_ + c(2)*A1_ + c(3)*A2_;
+	A = 	c(1)*A0_ - c(2)*A1_ + c(3)*A2_;
 	
 	row = zeros(size(A0_));
 	n = 5;
@@ -38,7 +38,7 @@ function [A, F] = global_matrix(N, c, f, BC, A0, A1, A2)
 	
 	% make F for A*U = F
 	% move A[0:2,:],  A[:,0:2],  A[-2:,:], A[:,-2:] to add to RHS
-	A_ = c(1)*A0 + c(2)*A1 + c(3)*A2;
+	A_ = c(1)*A0 - c(2)*A1 + c(3)*A2;
 	bc1 = BC(1)*A_(:,1) + BC(2)*A_(:,2);
 	bc2 = BC(3)*A_(:,3) + BC(4)*A_(:,4);
 	% create A0 to integrate f*v
